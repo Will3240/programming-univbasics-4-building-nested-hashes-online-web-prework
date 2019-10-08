f0c6faacb54e0857b4a33951551299f8e900e50e
William <teststuffreally@gmail.com>
2019-10-08 17:45:57 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -50,6 +50,7 @@ def monopoly_with_fourth_tier
       :shortline_railroad =>{}
     },
     "mortgage_value" = "$100"
+    
   }
 }
   # When you start writing the implementation for this method, copy the Hash
